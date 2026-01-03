f3a40c9e75e09da8ec2327852a9bca2bff676ee9
Carlos Cary <carloscary@192.168.1.7>
2026-01-02 21:48:46 -0400

    Initialize Cypress project

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  e2e: {
+
+    baseUrl: "https://demoqa.com",
+    video: false,
+    retries: {
+      runMode: 1,
+      openMode: 0
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
