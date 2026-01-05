26fc902b1d1e93ce5371246e6ec33dc7a5290285
Carlos Cary <carloscary@192.168.1.12>
2026-01-04 20:57:39 -0400

    refactor: move Practice Form test data to fixture and update gitignore file

--- cypress/e2e/forms/practiceForm.cy.js
+++ cypress/e2e/forms/practiceForm.cy.js
@@ -1,105 +1,99 @@
 describe("Practice Form", () => {
-
     beforeEach(() => {
-        cy.visit('/automation-practice-form');
+        cy.visit("/automation-practice-form");
+        cy.fixture("practiceForm").as("formData");
     });
 
-    it("submits the form successfully (happy path)", () => {
-
-        // Test data
-        const firstName = 'Carlos';
-        const lastName = 'Cary';
-        const email = 'carlos@test.com';
-        const mobile = '1234567890';
-        const gender = 'Male';
-        const birthYear = '1995';
-        const birthMonth = 'June';
-        const birthDay = '14';
-
-        const subject = 'Maths';
-        const hobby = 'Sports';
-
-        const address = '123 Cypress Street, Test City';
-
-        const state = 'NCR';
-        const city = 'Delhi';
-
-        const fixturesPath = 'cypress/fixtures';
-        const pictureName = 'profile.jpeg';
+    it("submits the form successfully (happy path)", function () {
+        const {
+            firstName,
+            lastName,
+            email,
+            mobile,
+            gender,
+            birthYear,
+            birthMonth,
+            birthDay,
+            subject,
+            hobby,
+            address,
+            state,
+            city,
+            fixturesPath,
+            pictureName,
+        } = this.formData;
 
         // Personal information
-        cy.get('#firstName').type(firstName).should('have.value', firstName);
-        cy.get('#lastName').type(lastName).should('have.value', lastName);
-        cy.get('#userEmail').type(email).should('have.value', email);
+        cy.get("#firstName").type(firstName).should("have.value", firstName);
+        cy.get("#lastName").type(lastName).should("have.value", lastName);
+        cy.get("#userEmail").type(email).should("have.value", email);
 
         // Gender selection
         cy.contains(gender).click();
 
         // Contact Details
-        cy.get('#userNumber').type(mobile).should('have.value', mobile);
+        cy.get("#userNumber").type(mobile).should("have.value", mobile);
 
         // Date of birth
-        cy.get('#dateOfBirthInput').click();
-        cy.get('.react-datepicker__year-select').select(birthYear);
-        cy.get('.react-datepicker__month-select').select(birthMonth);
-        cy.get('.react-datepicker__day').not('.react-datepicker__day--outside-month').contains(birthDay).click();
+        cy.get("#dateOfBirthInput").click();
+        cy.get(".react-datepicker__year-select").select(birthYear);
+        cy.get(".react-datepicker__month-select").select(birthMonth);
+        cy.get(".react-datepicker__day")
+            .not(".react-datepicker__day--outside-month")
+            .contains(birthDay)
+            .click();
 
         // Subjects
-        cy.get('#subjectsInput').type(`${subject}{enter}`);
+        cy.get("#subjectsInput").type(`${subject}{enter}`);
 
         // Hobbies
         cy.contains(hobby).click();
 
         // Profile picture
-        cy.get('#uploadPicture').selectFile(`${fixturesPath}/${pictureName}`);
-
+        cy.get("#uploadPicture").selectFile(`${fixturesPath}/${pictureName}`);
 
         // Address
-        cy.get('#currentAddress').type(address);
+        cy.get("#currentAddress").type(address);
 
         // State and city
-        cy.get('#state').click();
-        cy.get('#react-select-3-input').type(`${state}{enter}`);
+        cy.get("#state").click();
+        cy.get("#react-select-3-input").type(`${state}{enter}`);
 
-        cy.get('#city').click();
-        cy.get('#react-select-4-input').type(`${city}{enter}`);
+        cy.get("#city").click();
+        cy.get("#react-select-4-input").type(`${city}{enter}`);
 
-        cy.get('#submit').click();
+        cy.get("#submit").click();
 
         // Modal and assertions
-        cy.get('#example-modal-sizes-title-lg').should('be.visible');
-        cy.contains('td', 'Student Name')
+        cy.get("#example-modal-sizes-title-lg").should("be.visible");
+
+        cy.contains("td", "Student Name")
             .next()
-            .should('contain', `${firstName} ${lastName}`);
-        cy.contains('td', 'Student Email')
+            .should("contain", `${firstName} ${lastName}`);
+
+        cy.contains("td", "Student Email").next().should("contain", email);
+
+        cy.contains("td", "Gender").next().should("contain", gender);
+
+        cy.contains("td", "Mobile").next().should("contain", mobile);
+
+        cy.contains("td", "Date of Birth")
             .next()
-            .should('contain', email);
-        cy.contains('td', 'Gender')
+            .should("contain", birthDay)
+            .and("contain", birthMonth)
+            .and("contain", birthYear);
+
+        cy.contains("td", "Subjects").next().should("contain", subject);
+
+        cy.contains("td", "Hobbies").next().should("contain", hobby);
+
+        cy.contains("td", "Picture").next().should("contain", pictureName);
+
+        cy.contains("td", "Address").next().should("contain", address);
+
+        cy.contains("td", "State and City")
             .next()
-            .should('contain', gender);
-        cy.contains('td', 'Mobile')
-            .next()
-            .should('contain', mobile);
-        cy.contains('td', 'Date of Birth')
-            .next()
-            .should('contain', birthDay)
-            .and('contain', birthMonth)
-            .and('contain', birthYear);
-        cy.contains('td', 'Subjects')
-            .next()
-            .should('contain', subject);
-        cy.contains('td', 'Hobbies')
-            .next()
-            .should('contain', hobby);
-        cy.contains('td', 'Picture')
-            .next()
-            .should('contain', pictureName);
-        cy.contains('td', 'Address')
-            .next()
-            .should('contain', address);
-        cy.contains('td', 'State and City')
-            .next()
-            .should('contain', state)
-            .and('contain', city);
+            .should("contain", state)
+            .and("contain", city);
     });
 });
